3e1dc5a84717565c00d144da933665ec7fa5e997
Özgün Ergen <orhanozgun.ergen@kocsistem.com.tr>
2022-02-23 16:42:48 +0300

    spm fix

--- PhotoEditor/PhotoEditor+Font.swift
+++ PhotoEditor/PhotoEditor+Font.swift
@@ -15,10 +15,13 @@ extension PhotoEditorViewController {
     func registerFont() {
         let url = Bundle.bundle(forResource: "icomoon", ofType: "ttf").bundleURL
         
+        print("P1 ", url)
         guard let fontDataProvider = CGDataProvider(url: url as CFURL) else {
             return
         }
+        print("P2")
         guard let font = CGFont(fontDataProvider) else {return}
+        print("P3")
         var error: Unmanaged<CFError>?
         guard CTFontManagerRegisterGraphicsFont(font, &error) else {
             return
